64c62df3c6c64481cbccd5152d3e0d4e40778e2f
Ririena <142820574+Ririena@users.noreply.github.com>
2023-11-22 22:12:45 +0700

    Menambahkan Moduls Untuk Detail Card

--- src/app/components/CardProfile.jsx
+++ src/app/components/CardProfile.jsx
@@ -1,12 +1,17 @@
-// components/CardProfile.js
+
 import React from 'react';
 import Image from 'next/image'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { Skeleton } from '@nextui-org/react';
+import { Skeleton, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from '@nextui-org/react';
 import { useState, useEffect} from 'react'
 
 const CardProfile = ({ user }) => {
+  
+  const [bukaModal , setBukaModal] = useState(false)
+
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
   useEffect(() => {
     AOS.init();
   }, []);
@@ -29,9 +34,37 @@ const CardProfile = ({ user }) => {
         <div data-aos="fade-up"></div>
         {user ? (
           <>
-            <h2 className="text-xl font-semibold text-gray-800">{user.name || 'nama tidak tersedia'}</h2>
-            <p className="text-gray-600">Motto Hidup: {user.email || 'Tidak Memiliki Motto Hidup'}</p>
-            <button className='mt-4 border px-4 py-2 rounded-lg border-amber-500 text-amber-500 hover:text-purple-600 hover:border-purple-600 hover:bg-amber-200'>Detail</button>
+            <h2 className="text-xl font-semibold text-gray-800">{user.name || 'Nama Tidak Ditemukan'}</h2>
+            <p className="text-gray-600">Motto Hidup: {user.email || 'Tidak Memiliki Motto  Hidup'}</p>
+            <Button onPress={onOpen} className='mt-4 border px-4 py-2 rounded-lg border-amber-500 text-amber-500 hover:text-purple-600 hover:border-purple-600 hover:bg-amber-200'>Detail</Button>
+  <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+    <ModalContent>
+      {(onClose) => (
+        <>
+        <ModalHeader className='bg-purple-600 text-white'>
+          Detail Pengguna
+        </ModalHeader>
+        <ModalBody className='bg-slate-400 shadow-md w-full'>
+          <Image
+            src={`/profile/${user.avatar}`}
+            alt={user.name}
+            className="rounded-full mx-auto"
+            width={120}
+            height={120}
+            style={{ objectPosition: 'center center' }}
+          />
+          <h2 className="text-xl font-semibold text-gray-800">{user.name || 'Nama Tidak Ditemukan'}</h2>
+          <p className="text-white">Motto Hidup: {user.email || 'Tidak Memiliki Motto Hidup'}</p>
+          <p className="text-white">Jenis Kelamin: {user.gender ? 'Laki-Laki' : 'Perempuan'}</p>
+          <p className="text-white">Alamat: {user.address || 'Alamat Tidak Ditemukan'}</p>
+        </ModalBody>
+        <ModalFooter className='bg-purple-600'>
+          <Button  className='text-white font-semibold' onPress={() => { onClose(); setBukaModal(false); }}>Tutup</Button>
+        </ModalFooter>
+        </>
+      )}
+    </ModalContent>
+  </Modal>
           </>
         ) : (
           <>
